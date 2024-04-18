baa1757540eb9807e1f0eb01e8b8f18ef38ca67b
Skole <Skole@DESKTOP-87FGBUN>
2024-04-18 08:23:01 +0200

    Done

--- Øvelse 5.7/Program.cs
+++ Øvelse 5.7/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+for (int flasker = 99; flasker > 0; flasker--)
+{
+    Console.WriteLine($"{flasker} bottles of beer on the wall, {flasker} bottles of beer.");
+    Console.WriteLine($"Take one down and pass it around, {flasker - 1} bottles of beer on the wall.");
+    Console.WriteLine();
+
+}
